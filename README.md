e17cee1c53f2ebfa5512f522d184a0ed8e3155a2
kfda <kfda89@gmail.com>
2018-01-28 00:02:19 +0200

    editing the comments insode the script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-creating mini Calc By using html + bootstrap + javascript.
+creating mini Calc By using html + bootstrap + javascript(typeScript).
 
 Decription
 ==========
@@ -16,4 +16,4 @@ open the index.html file with your browser.
 Updates
 =======
 a few bugs will Appeared by using '()' or 'argements' > 2 in the Input.
-not finished yet... still working on it.
+not finished yet... still tying to understand out how to do that.
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//with those functions i can take have OP and calc with a string note
+// like '+' will be +
 var functions = {
   '+': function (a,b){return  a+b},
   '-': function (a,b){return  a-b},
@@ -17,28 +20,37 @@ const OP = [];
 const B = [];
 let index = 0;
   for(char of str){
+    //asking if the char is number
+    //if yes , add it to tmp
     if (Number.isInteger(Number(char))) {
       console.log('Number');
       temp += char;
       console.log('temp' , temp);
     }else if (char == '+' || char == '-' || char == '*' || char == '/' || char == '^') {
       console.log('soft char');
+      //A flag is up (true) by Default
+      //if it's an OP
       if (isA) {
+        //if it an opperator , put the temp inside A stack
+        // AND push the OP inside the OP
         console.log('isA: true');
         A.push(temp);
         OP.push(char);
+        //clear the tmp and wait for another char
         temp = '';
         isA = false;
-        isB = true;
         // console.log(isA);
       }else{
+        //if the A flag is not up (false) , asouming it's B
         console.log('isA: false , isB');
         B.push(temp);
         OP.push(char);
+        // after we have A , OP, B  - we are ready to calc
+
         time_to_calc = true;
       }
     }if(!isA && temp != '') time_to_calc= true;
-
+    //the index show us if the STRING is done.
     if (time_to_calc && index == str.length -1) {
       console.log('time to calc');
       if (temp != ''){
@@ -53,10 +65,10 @@ let index = 0;
     index ++;
     console.log('A:' , A , 'B:' , B , 'OP' , OP);
   }
+  //CANT Dividing By 0
   if (A[0] == Infinity) {
     $('#result').text('Error - cant Div by 0!');
   }else {
-
     $('#result').text(A.pop());
   }
 }
